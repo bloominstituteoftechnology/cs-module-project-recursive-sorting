158a9ccaaf1abd088eb9157040663984f6d2f299
marilynle <marilynle@users.noreply.github.com>
2020-06-05 19:41:41 -0500

    'assignment'

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -2,32 +2,73 @@
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-
-    # Your code here
-
-
+    a = 0
+    b = 0
+    c = 0
+    while a < len(arrA) and b < len(arrB):
+        if arrA[a] < arrB[b]:
+            merged_arr[c] = arrA[a]
+            c = c + 1
+            a = a + 1
+        else:
+            merged_arr[c] = arrB[b]
+            c = c + 1
+            b = b + 1
+    while a < len(arrA):    
+        merged_arr[c] = arrA[a]
+        c = c + 1
+        a = a + 1   
+    while b < len(arrB):   
+        merged_arr[c] = arrB[b]
+        c = c + 1
+        b = b + 1
+   
+    
     return merged_arr
-
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
-    # Your code here
-
-
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+        return merge(merge_sort(left), merge_sort(right))
+        
     return arr
 
 
 # implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
-    # Your code here
-
+    lst =[]
+    i = start
+    j = mid
+    while i < mid and j < end:
+        if arr[i] < arr[j]:
+            lst.append(arr[i])
+            i +=1
+        else:
+            lst.append(arr[j])
+            j +=1
+    while i < mid:
+        lst.append(arr[i])
+        i +=1
+    #while j < end:
+     #   lst.append(arr[j])
+      #  j +=1                
+    for i in range(len(lst)):
+        arr[start + i] = lst[i]
 
     return arr
 
 
 def merge_sort_in_place(arr, l, r):
-    # Your code here
+    if l >= r - 1:
+        return
+    mid = (l + r) // 2
 
+    merge_sort_in_place(arr, l, mid)
+    merge_sort_in_place(arr, mid, r)
+    merge_in_place(arr, l, mid, r)
 
     return arr
 
