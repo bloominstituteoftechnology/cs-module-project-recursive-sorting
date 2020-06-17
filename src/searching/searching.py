50490d7c5762a8342e27a648218ee57d962845c9
hyamynl619 <amy.nguyenle619@gmail.com>
2020-06-17 14:19:02 -0400

    adding mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,29 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    # binary search algorith is supposed to work with ordered arrays
+    
+    # Base case
+    # The start point is greater or equal to the end point
+    if end >= start:
+        # declare a the middle point
+        middle = (start + end) // 2
+
+        # if the target is present at the middle,
+        # return middle
+        if arr[middle] == target:
+            return middle
+        
+        # if the target is smaller than the middle
+        # then it's in the left subarray
+        elif arr[middle] > target:
+            # recurse
+            return binary_search(arr, target, start, middle-1)
+        # if the target is greater or equal to the middle
+        # it's in the right subarray
+        else:
+            return binary_search(arr, target, middle+1, end)
+    else:
+        return -1   # target not found
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -11,4 +34,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
