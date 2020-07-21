1105befa6fb8c1659b5db904dd80d76ce8da4545
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-07-21 15:30:10 -0400

    Completes recursive binary search with test passing

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,36 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
+    """
+    Returns either the index of target in arr,
+    or -1 if target is not in arr
+    """
     # Your code here
+    
+    # how to move closer to base case
+    # if starting index is less than ending index
+    if start <= end:
+        # Use conditions from iterative implementation
+        mid = (end + start) // 2
+        
+        # found target
+        if target == arr[mid]:
+            return mid
+
+        # go left and apply binary_search(new end point)
+        if target < arr[mid]:
+            return binary_search(arr, target, start, mid-1)
+
+        # go right and apply binary_search(new start point)
+        if target > arr[mid]:
+            return binary_search(arr, target, mid+1, end)
+        
+    # otherwise
+    # base case (stopping point)
+    # target not in arr
+    else:
+        # if value at start is 
+        # target not in arr
+        return -1
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -11,4 +41,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
