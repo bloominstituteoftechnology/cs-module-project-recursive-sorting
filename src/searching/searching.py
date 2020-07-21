8f6f110e3d2245b4bd22821b4ca8456bf73faa45
Ben <bbjberg91@gmail.com>
2020-07-21 19:15:46 -0400

    mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -2,6 +2,19 @@
 def binary_search(arr, target, start, end):
     # Your code here
 
+    if end >= start:
+        mid = (start + end) // 2
+
+        if arr[mid] == target:
+            return mid
+
+        elif arr[mid] > target:
+            return binary_search(arr, target, start, mid)
+        else:
+            return binary_search(arr, target, mid , end)
+        
+    else:
+        return -1
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -11,4 +24,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
