df9cffa2d27538ec0376a211afca0d7d8aff1251
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-07-22 13:33:17 -0400

    Class III

---
+++ practice/c_th.py
@@ -0,0 +1,13 @@
+def c_th(string):
+    # base case
+    # entire string searched
+    # need way to progress through string
+    
+    pass
+    # how to get closer to base case
+
+if __name__ == "__main__":
+    my_string = "Hello world, it's a new day"
+    # s1 = slice(1)
+    # print(my_string[s1])
+    breakpoint()
