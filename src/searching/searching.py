f067ec986a3fa9975ba9b1fe41bd4c837d07dca0
chrishuskey <chrishuskey@users.noreply.github.com>
2020-07-22 13:24:34 -0400

    Add searching.py (recursive implementations of binary search algo).

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,26 @@
-# TO-DO: Implement a recursive implementation of binary search
-def binary_search(arr, target, start, end):
-    return
+def binary_search(array, target, start, end):
+    """
+    Search the input array for the target value using a recursive version 
+    of the binary search algorithm.
+    """
+    midpoint = (start + end) // 2
+    # Base case: If no items in subarray, return False:
+    if end < start:
+        return -1
+    
+    # Otherwise, recursive search to get to the base case:
+    if array[midpoint] == target:
+        return midpoint
+    elif target < array[midpoint]:
+        return binary_search(array=array, 
+                             target=target, 
+                             start=start, 
+                             end = midpoint - 1)
+    elif target > array[midpoint]:
+        return binary_search(array=array, 
+                             target=target, 
+                             start = midpoint + 1, 
+                             end=end)
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -9,5 +29,5 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
+def agnostic_binary_search(array , target):
     return
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,4 +1,72 @@
-# TO-DO: complete the helper function below to merge 2 sorted arrays
+'''
+Out-of-place Quicksort
+'''
+def partition_for_quicksort(data):
+    # Partition the data
+    # Start by choosing a pivot (choose the first item in the list)
+    pivot = data[0]
+    # We need to create storage for the LHS and the RHS
+    left = []
+    right = []
+​
+    # We need to loop through each item
+    for current in data[1:]:
+        # if it's smaller or equal, append to left
+        if current <= pivot:
+            left.append(current)
+        # if it's bigger, add to RHS storage
+        else:
+            right.append(current)
+​
+    return left, right, pivot
+
+
+# What kind of input will we get?
+# We expect a list
+def quicksort_out_of_place(data):
+    # check if data has 1 or 0 elements
+    # (base case) a side only contains a single element
+    if len(data) <= 1:
+        return data
+​
+    left, right, pivot = partition_for_quicksort(data)
+​
+    # (recursive case) Recursively Quick Sort LHS and RHS until
+    return quicksort(left) + [pivot] + quicksort(right)
+
+def quicksort_in_place(data, low, high):
+    # base case
+    if low >= high:
+        return data
+    # recursive case
+    else:
+        # divide
+        pivot_index = low
+        # for each element in subarray
+        for i in range(low, high):
+            if data[i] < data[pivot_index]:
+                # double swap to move smaller elements to correct index
+                # move current element to the right of pivot
+                temp = data[pivot_index+1]
+                data[pivot_index+1] = data[i]
+                data[i] = temp
+​
+                # swap pivot with element on its right
+                temp = data[pivot_index]
+                data[pivot_index] = data[pivot_index+1]
+                data[pivot_index+1] = temp
+                pivot_index += 1
+​
+        # conquer
+        # Quick Sort everything left of the pivot
+        data = quick_sort(data, low, pivot_index)
+        # Quick Sort everything right of the pivot
+        data = quick_sort(data, pivot_index+1, high)
+  
+        return data
+
+
+# -------------------------------------------------------------------------
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
