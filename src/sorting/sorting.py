63dba01c62451bfdfc7775f152f3d77f936cc195
Richard Olson <8571182+richardOlson@users.noreply.github.com>
2020-08-05 22:35:26 -0600

    working on the sorting

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -62,39 +62,96 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # doing the merge_in_place
-    rStart = mid
-    l = start
+    # creating a base case
+    if start == end:
+        return
+    breakpoint()
+    lpt = mid -1
+    rpt = mid
+    need_rotate = False
+    # this is the case if there are just two items
+    if lpt == start:
+        if arr[lpt] > arr[rpt]:
+            temp = arr[lpt]
+            arr[lpt] = arr[rpt]
+            arr[rpt] = temp
+        return
+    # tyring to do the merge in place
+    # pick the middle and move outward till we 
+    # find one on the l side that is less
+    # this here is for when there are more than 2 in length
+    while lpt >=start and rpt <= end:
+        if lpt < rpt:
+            need_rotate = True
+            break
+        # move them closer to the middle
+        lpt -=1
+        rpt +=1
+    # Will rotate those indices that are between the lpt and the rpt
+    if need_rotate == False:
+        return 
 
-    while l < mid and rStart < end +1:
-        # doing the swaping if it is needed
-        if arr[rStart] < arr[l]:
-            #  put the r into the left side
-            temp = arr[l]
-            arr[l] = arr[rStart]
-            arr[rStart] = temp
-            # will then need to increment the l
-            
+    # Will do the swapping of the elements
+    if lpt == mid-1:
+        swapto = lpt
+        swapfrom =rpt
+    else:
+        swapto = lpt +1
+        swapfrom = mid
+    while need_rotate:
+        temp = arr[swapto]
+        arr[swapto] = arr[swapfrom]
+        arr[swapfrom] = temp
+        swapfrom +=1
+        swapto += 1
+        if swapto >= mid:
+            need_rotate = False
+    # calling this same call again recursively for the left and the right side
+    # left side
+    #lend =mid -1
+    #l_mid = start + (1+(lend - start ))//2
+    #r_mid = mid + (1 + (end - mid))//2
+    #merge_in_place(arr, start=start,  mid=l_mid, end=lend)
+    #merge_in_place(arr, start=mid, mid=r_mid, end=end)
+    #breakpoint()
+    #while l < mid or end >= mid:
+    #    # doing the swaping if it is needed
+    #    if arr[mid] < arr[l] and l < mid:
+    #        #  put the r into the left side
+    #        temp = arr[l]# 
+    #        arr[l] = arr[mid]
+    #        arr[mid] = temp
+    #        # will then need to increment the l
+    #        
+    #    if arr[end] < arr[leftBig] and end >= mid:
+    #        # will be doing the swapping of the
+    #        # to put the largest number/item in 
+    #        # the end. and then will decrement the 
+    #        # end.
+    #        temp = arr[end]
+    #        arr[end] = arr[leftBig]
+    #        arr[leftBig] = temp
+    #    end -=1
+    #    l +=1
         
-        l +=1
+        
+        
             
 
 def merge_sort_in_place(arr, l, r):
     # the base case is when the left and the right are equal to 
     # each other
     if l == r:
-        return l,r
-    
+        return  l,r
     # we will find the middle point where we want to split the arr
     length = 1 + (r - l)
     mid =  l + (length//2)
-    # will then be calling the merge_sort_in_place on each of the halves
+   # will then be calling the merge_sort_in_place on each of the halves
     leftStart ,lend = merge_sort_in_place(arr, l, r=mid-1)
     rstart, rightend = merge_sort_in_place(arr, l=mid, r=r)
-    # Will now call the merge_in_place function
-    # start is the left start, mid will be the rightStart, end will be right end
-    
-    mid = (1 + (rightend - leftStart)) //2
-
+   # Will now call the merge_in_place function
+   # start is the left start, mid will be the rightStart, end will be right end
+   
+    mid = leftStart + ((1 + (rightend - leftStart)) //2)
     merge_in_place(arr, start=leftStart, mid=rstart, end=rightend)
-
     return leftStart, rightend
