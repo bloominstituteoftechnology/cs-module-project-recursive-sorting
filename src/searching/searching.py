3b50e302ab3f6d404749809a5ec47c2847d44968
CyrusTheCurator <joshua.c.luscombe@gmail.com>
2020-08-07 21:13:11 -0700

    add ultra-complex list solve

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,20 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    middle = (start+end)//2
+    if arr[middle] == target:
+        return middle
+    elif  target < arr[middle]:
+        middle = binary_search(arr,target,start,middle-1)
+    elif target > arr[middle]:
+        middle = binary_search(arr,target,middle+1,end)
+    return middle
+
+listy = [1,2,3,4,5,6,7,8,9]
+
+print(binary_search(listy, 8, 0, 8))
+    
+
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -9,6 +23,6 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
+# def agnostic_binary_search(arr, target):
     # Your code here
 
