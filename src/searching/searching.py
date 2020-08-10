d7d5f0218cf4fe7da55a756fea9bfbf267ec8035
David Wright <davidtwright89@gmail.com>
2020-08-10 00:43:19 -0400

    Implemented stretch function: agnostic_binary_search()

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -19,5 +19,24 @@ def binary_search(arr, target, start, end):
 # You can implement this function either recursively 
 # or iteratively
 def agnostic_binary_search(arr, target):
-    # Your code here
-    pass
+    if len(arr) == 0:
+        return -1
+    elif len(arr) == 1:
+        return 0 if (arr[0] == target) else -1
+    
+    if arr[0] < arr[1]:
+        return binary_search(arr, target, 0, len(arr) - 1)
+    else:
+        return binary_search_descending(arr, target, 0, len(arr) - 1)
+
+def binary_search_descending(arr, target, start, end):
+    if end < start:
+        return -1
+    
+    middle = (end + start) // 2
+    if arr[middle] == target:
+        return middle
+    elif arr[middle] < target:
+        return binary_search_descending(arr, target, start, middle - 1)
+    else:
+        return binary_search_descending(arr, target, middle + 1, end)
