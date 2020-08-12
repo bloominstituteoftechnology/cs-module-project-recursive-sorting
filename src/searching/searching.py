0b3dd21d26ea5511ae635918b3174b74c4d21b01
quan005 <ralphealfreeman@gmail.com>
2020-08-11 18:29:09 -0700

    mvp complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,24 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    middle = (low+high)//2
+
+    if len(arr) == 0:
+        return -1 # array empty
+    # TO-DO: add missing if/else statements, recursive calls
+    else:
+        if arr[middle] == target:
+            # If so, return True or 1 or the index
+            print('found at', middle)
+            return middle
+        # Check if the middle point is greater than the target
+        elif arr[middle] > target:
+            # If so, change high to equal the middle point minus 1
+            return binary_search_recursive(arr, target, low, middle-1)
+        # Check if the middle point is less than the target
+        elif arr[middle] < target:
+            # If so, change low to equal the middle point plus 1
+            return binary_search_recursive(arr, target, middle + 1, high)
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -11,4 +29,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
