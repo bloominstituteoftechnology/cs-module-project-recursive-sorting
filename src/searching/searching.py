0a6f70894c9d6bc131dd7fb6a910b686752504d0
Lambda_School_Loaner_277 <lambda_school_loaner_277@Lambda-School-Loaner-277s-MacBook-Pro.local>
2020-08-18 20:13:55 -0400

    Complete and Working

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,22 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    while start <= end:
+        middle = (start + end) // 2
+        if target < arr[middle]:
+            return binary_search(arr, target, start, (middle - 1))
+        elif target > arr[middle]:
+            return binary_search(arr, target, (middle + 1), end)
+        else:
+            return middle
+    return -1  # not found
 
 
 # STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
+# This version of binary search should correctly find
 # the target regardless of whether the input array is
 # sorted in ascending order or in descending order
-# You can implement this function either recursively 
+# You can implement this function either recursively
 # or iteratively
-def agnostic_binary_search(arr, target):
+# def agnostic_binary_search(arr, target):
     # Your code here
-
