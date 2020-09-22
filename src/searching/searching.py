ab476bfc95b5e81e43ea1ffde8bc4cf4164c5ef6
Kelson Hartle <kelson99hartle@icloud.com>
2020-09-22 09:50:09 -0600

    Binary search tests passing

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -2,6 +2,30 @@
 def binary_search(arr, target, start, end):
     # Your code here
 
+    low = start
+    high = end
+
+    if len(arr) == 0:
+        return -1
+
+    # if low <= high:
+    middle = (low + high) // 2
+    guess = arr[middle]
+        
+    if guess == target:
+        print(middle)
+        return middle
+
+    elif guess > target:
+        return binary_search(arr, target, low, high - 1)
+
+    else: 
+        return binary_search(arr, target, low + 1, high)
+
+    return -1
+
+    
+
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -9,6 +33,6 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
-    # Your code here
+# def agnostic_binary_search(arr, target):
+#     # Your code here
 
