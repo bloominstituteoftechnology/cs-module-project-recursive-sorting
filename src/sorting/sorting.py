a273ef517370dd7d2b8fc6561fe4494d56ddfcf6
Conner <0xconr@protonmail.com>
2020-10-01 14:44:39 -0500

    sorting.py: merge() complete but messy

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,17 +1,47 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+	elements = len(arrA) + len(arrB)
+	merged_arr = []
 
-    # Your code here
+	a_index = 0
+	b_index = 0
+	
+	for i in range(0, elements):
+		print('merged_arr =', merged_arr)
+		print('i =', i)
+		print('a_index =', a_index)
+		print('b_index =', b_index)
 
+		# OOB check A
+		if a_index == len(arrA):
+			# arrA is depleted, append all contents of arrB to merged_arr
+			while b_index != len(arrB):
+				merged_arr.append(arrB[b_index])
+				b_index += 1
+			return merged_arr
 
-    return merged_arr
+		# OOB check B
+		if b_index == len(arrB):
+			while a_index != len(arrA):
+				merged_arr.append(arrA[a_index])
+				a_index += 1
+			return merged_arr
 
-# TO-DO: implement the Merge Sort function below recursively
+		# A <= B
+		if arrA[a_index] <= arrB[b_index]:
+			merged_arr.append(arrA[a_index])
+			a_index += 1
+
+		# A > B 
+		else:
+			merged_arr.append(arrB[b_index])
+			b_index += 1
+
+	return merged_arr
+
+"""
 def merge_sort(arr):
-    # Your code here
-
+	#m =
 
     return arr
 
@@ -25,4 +55,12 @@ def merge_in_place(arr, start, mid, end):
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
+"""
 
+if __name__ == "__main__":
+	arr1 = [3, 6, 9]
+	arr2 = [6, 2000, 4000]
+	print('arr1 = ', arr1)
+	print('arr2 = ', arr2)
+	arr3 = merge(arr1, arr2)
+	print('arr3 = ', arr3)
