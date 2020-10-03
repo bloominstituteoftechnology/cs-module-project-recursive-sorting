cc5f9bab72cd07a4ddf8c1e24fee8ce22400db6a
Claudia Maciel <clc_80@yahoo.com>
2020-10-02 18:47:25 -0700

    Binary search passing tests

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,28 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    #base case if the first the same as the last
+    if start > end:
+        return -1
+
+    # get the middle
+    middle = (start + end) // 2
+
+    # base case (item found in middle spot)
+    if target == arr[middle]:
+        return middle
+
+    else:
+        #  if not found, split in half depending on which side it is
+        if target < arr[middle]:
+            #arr = arr[:middle]
+            # change the last if on the left
+            end = middle - 1
+        else:
+            # change the first if on the right
+            #arr = arr[]
+            start = middle + 1
+        # call itself
+        return binary_search(arr, target, start, end)
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -11,4 +33,8 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
+    pass
 
+arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+
+print(binary_search(arr1, -8, 0, len(arr1)-1))
