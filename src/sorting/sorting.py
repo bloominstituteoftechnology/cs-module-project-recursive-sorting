ec84d5e957872f48f666135cb5e0cc7c3a64d11b
Chad Parker <chad@chadparker.com>
2020-10-03 19:45:30 -0700

    Implement merge() helper function

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -3,10 +3,32 @@ def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
 
-    # Your code here
-
+    while elements:
+        if arrA and arrB:
+            if arrA[0] < arrB[0]:
+                # assign to merged_arr at correct index
+                merged_arr[-elements] = arrA[0]
+                # remove from A
+                del arrA[0]
+                elements -= 1
+            else:
+                merged_arr[-elements] = arrB[0]
+                del arrB[0]
+                elements -= 1
+        else:
+            # one array empty, one with 1+ elements left
+            if arrA:
+                merged_arr[-elements] = arrA[0]
+                del arrA[0]
+                elements -= 1
+            if arrB:
+                merged_arr[-elements] = arrB[0]
+                del arrB[0]
+                elements -= 1
 
     return merged_arr
+
+print(merge([1, 4, 5, 9], [2, 3, 6, 7, 8, 10]))
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
@@ -19,10 +41,10 @@ def merge_sort(arr):
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
+# def merge_in_place(arr, start, mid, end):
     # Your code here
 
 
-def merge_sort_in_place(arr, l, r):
+# def merge_sort_in_place(arr, l, r):
     # Your code here
 
