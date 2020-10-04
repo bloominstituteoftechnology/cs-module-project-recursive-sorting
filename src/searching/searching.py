a94309c2eaa0f41619eb51321a3670e3998f270a
Chad Parker <chad@chadparker.com>
2020-10-03 21:50:59 -0700

    Implement stretch: agnostic_binary_search()

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -25,6 +25,30 @@ binary_search([3, 5, 8], 1, None, None)
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-# def agnostic_binary_search(arr, target):
-#     # Your code here
-#     pass
+
+def agnostic_binary_search(arr, target):
+    
+    first = 0
+    last = len(arr) - 1
+    index = -1 # not found (yet)
+
+    while first <= last and index < 0:
+        middle = (first + last) // 2
+
+        if arr[middle] == target:
+            index = middle
+        else:
+            # ascending
+            if arr[0] < arr[last]:
+                if target < arr[middle]:
+                    last = middle - 1
+                else:
+                    first = middle + 1
+            # descending
+            else:
+                if target > arr[middle]:
+                    last = middle - 1
+                else:
+                    first = middle + 1
+                    
+    return index
