a4255e84aacdc1fbb23478e0c8d7b540d25eecaa
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-10-05 13:06:54 -0400

    Implement recursive merge_sort()

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -3,26 +3,60 @@ def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
 
-    # Your code here
+    current_index = 0
+    arrA_index = 0
+    arrB_index = 0
 
+    while current_index < elements:
+        if arrB_index < len(arrB) and arrA_index < len(arrA):
+            if arrA[arrA_index] > arrB[arrB_index]:
+                merged_arr[current_index] = arrB[arrB_index]
+                arrB_index += 1
+
+            else:
+                merged_arr[current_index] = arrA[arrA_index]
+                arrA_index += 1
+
+        elif arrB_index < len(arrB):
+            merged_arr[current_index] = arrB[arrB_index]
+            arrB_index += 1
+        else:
+            merged_arr[current_index] = arrA[arrA_index]
+            arrA_index += 1
+
+        current_index += 1
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
-def merge_sort(arr):
-    # Your code here
 
+# Divide and conquer strategy
+
+
+def merge_sort(arr):
+
+    if len(arr) > 1:
+        midpoint = len(arr) // 2  # Integer division
+
+        left = arr[:midpoint]
+
+        right = arr[midpoint:]
+
+        return merge(merge_sort(left), merge_sort(right))  # Recursion
 
     return arr
 
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
+# STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
+# In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
+
+
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    pass
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+    pass
