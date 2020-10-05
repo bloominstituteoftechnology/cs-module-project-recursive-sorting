2c8b668831dd2ee7e44ffa15097e0b5ee71f52db
Matt Martindale <martindalematt18@yahoo.com>
2020-10-04 21:15:13 -0600

    Refactored Sorting.py

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -3,8 +3,29 @@ def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
 
-    # Your code here
+    left_array = 0
+    right_array = 0
+    index = 0
 
+    while left_array < len(arrA) and right_array < len(arrB):
+        if arrA[left_array] < arrB[right_array]:
+            merged_arr[index] = arrA[left_array]
+            left_array += 1
+        else:
+            merged_arr[index] = arrB[right_array]
+            right_array += 1
+        
+        index += 1
+
+    while left_array < len(arrA):
+        merged_arr[index] = arrA[left_array]
+        left_array += 1
+        index += 1
+
+    while right_array < len(arrB):
+        merged_arr[index] = arrB[right_array]
+        right_array += 1
+        index += 1
 
     return merged_arr
 
@@ -12,35 +33,21 @@ def merge(arrA, arrB):
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-  if len(arr)>1:
-    mid = len(arr)//2
-    left = arr[:mid]
-    right = arr[mid:]
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
 
-    left = merge_sort(left)
-    right = merge_sort(right)
+        left = merge_sort(left)
+        right = merge_sort(right)
 
-    arr = []
+        return merge(left, right)
 
-    while len(left) > 0 and len(right) > 0:
-      if left[0] < right[0]:
-        arr.append(left[0])
-        left.pop(0)
-      else:
-        arr.append(right[0])
-        right.pop(0)
-
-    for i in left:
-      arr.append(i)
-    for i in right:
-      arr.append(i)
-
-  return arr
+    return arr
 
 arr = [5, 3, 7, 3, 6, 12, 15, 53]
 print(arr)
-arr1 = merge_sort(arr)
-print(arr1)
+print(merge_sort(arr))
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
@@ -54,4 +61,19 @@ def merge_in_place(arr, start, mid, end):
 def merge_sort_in_place(arr, l, r):
     # Your code here
     pass
+
+# arr = []
+
+    # while len(left) > 0 and len(right) > 0:
+    #   if left[0] < right[0]:
+    #     arr.append(left[0])
+    #     left.pop(0)
+    #   else:
+    #     arr.append(right[0])
+    #     right.pop(0)
+
+    # for i in left:
+    #   arr.append(i)
+    # for i in right:
+    #   arr.append(i)
 
