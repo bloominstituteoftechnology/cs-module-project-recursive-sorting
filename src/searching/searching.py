a309f6fcdfd494890449587cca1c740f4dd3f4eb
FabiolaSaga <saga.fabiola@gmail.com>
2020-10-05 21:21:13 -0500

    Finished MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,19 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    if start > end:
+        return - 1
+    
+    mid = (start + end) // 2
 
+    if target == arr[mid]:
+        return mid
+    
+    if target < arr[mid]:
+        return binary_search(arr, target, start, mid - 1)
+
+    if target > arr[mid]:
+        return binary_search(arr, target, mid + 1, end)
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -11,4 +23,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -4,16 +4,44 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
+    indexA = indexB = indexM = 0
 
+    while indexA < len(arrA) and indexB < len(arrB):
+        if arrA[indexA] < arrB[indexB]:
+            merged_arr[indexM] = arrA[indexA]
+            indexA += 1
+        else:
+            merged_arr[indexM] = arrB[indexB]
+            indexB += 1
+        indexM += 1
+    
+    while indexA < len(arrA):
+        merged_arr[indexM] = arrA[indexA]
+        indexA += 1
+        indexM += 1
+    
+    while indexB < len(arrB):
+        merged_arr[indexM] = arrB[indexB]
+        indexB += 1
+        indexM += 1
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
 
+    left = arr[:mid]
+    left = merge_sort(left)
 
-    return arr
+    right = arr[mid:]
+    right = merge_sort(right)
+
+    return merge(left, right)
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
@@ -21,8 +49,9 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
-
+    pass
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
+    pass
 
