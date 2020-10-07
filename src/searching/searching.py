1a95d19204d3c653e79411035c618c4f12477be2
Alex Thompson <guitarman226@gmail.com>
2020-10-06 20:55:43 -0500

    Completed mvp
    Completed the binary search in searching.py, and completed the sorting methods

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,36 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
+
     # Your code here
+    if start is None and end is None:
+        start = 0
+        end = len(arr)
+        
+    if start > end:
+        return -1
+    
+    else:
+        mid = (start + end) // 2
+        
+        if arr[mid] == target:
+            return mid
+        
+        elif arr[mid] > target:
+            return binary_search(arr, target, start, mid - 1)
+        
+        else:
+            return binary_search(arr, target, mid + 1, end)
+            
+# Testing!!
+arr = [2, 3, 4, 10, 40]
+target = 1
+
+result = binary_search(arr, 0, len(arr)-1, target)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in the array")
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -9,6 +39,5 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
-    # Your code here
-
+# def agnostic_binary_search(arr, target):
+ # Your code here
