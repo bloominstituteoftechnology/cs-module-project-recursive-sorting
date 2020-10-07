6fb0ff9698311ab865d73822aaf2fda397675486
Anthony <chitowncoder@gmail.com>
2020-10-06 20:55:46 -0500

    MVP met

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,27 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    if start > end:
+        return - 1
+
+    mid = (start + end) // 2
+
+    if target == arr[mid]:
+        return mid
+
+    elif target < arr[mid]:
+        return binary_search(arr, start, mid - 1, target)
+
+    else:
+        return binary_search(arr, mid + 1, end, target)
 
 
 # STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
+# This version of binary search should correctly find
 # the target regardless of whether the input array is
 # sorted in ascending order or in descending order
-# You can implement this function either recursively 
+# You can implement this function either recursively
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,28 +1,80 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
+
+def create_array(size=8, max=50):
+    from random import randint
+    return [randint(0, max) for _ in range(size)]
+
+# TO-DO: implement the Merge Sort function below recursively
+
+
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
 
-    # Your code here
+    arrA_pointer = arrB_pointer = 0
 
+    arrA_length, arrB_length = len(arrA), len(arrB)
+
+    for _ in range(elements):  # 8 elements of at value of 0 array
+        if arrA_pointer < arrA_length and arrB_pointer < arrB_length:  # base case
+            if arrA[arrA_pointer] <= arrB[arrB_pointer]:
+                merged_arr[arrA_pointer + arrB_pointer] = arrA[arrA_pointer]
+                arrA_pointer += 1
+
+            else:
+                merged_arr[arrA_pointer + arrB_pointer] = arrB[arrB_pointer]
+
+                arrB_pointer += 1
+
+        elif arrA_pointer == arrA_length:
+            # right array
+            merged_arr[arrA_pointer + arrB_pointer] = arrB[arrB_pointer]
+            arrB_pointer += 1
+
+        elif arrB_pointer == arrB_length:
+            merged_arr[arrA_pointer +
+                arrB_pointer] = arrA[arrA_pointer]  # left array
+            arrA_pointer += 1
 
     return merged_arr
 
-# TO-DO: implement the Merge Sort function below recursively
+
+# test case
+a = [1, 3, 5]
+b = [2, 4, 6]
+
+
 def merge_sort(arr):
-    # Your code here
+    if len(arr) <= 1:
+        return arr
+
+    midpoint = int(len(arr) // 2)
+
+    arrA = merge_sort(arr[:midpoint])
+    arrB = merge_sort(arr[midpoint:])
+
+    return merge(arrA, arrB)
 
 
-    return arr
+# verify it works
+a = create_array()
+b = create_array()
+print(a)
+print(b)
 
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
+s = merge_sort(a + b)
+print(s)
+
+
+# STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
+# In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    pass
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+    pass
